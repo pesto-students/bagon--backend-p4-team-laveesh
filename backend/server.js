c672f46076a02dbe304324ed4477a8b27e2974bb
Sumanbitw <007sumanghosh@gmail.com>
2023-01-07 23:55:27 +0530

    error handlers and generating token function

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,21 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const PORT = 3000;
+const userRoute = require("../backend/routes/userRoute");
+const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 
 const app = express();
 dotenv.config();
 connectDB();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running successfully");
+});
+
+app.use("/api/users", userRoute);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, console.log(`PORT connceted to ${PORT}`));
